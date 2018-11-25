4a05ae00ff0edb91e090613e6c68c1bd1df7b75c
Dennis Hermsmeier <dennis.hermsmeier@gmail.com>
2018-11-25 18:40:49 +0100

    first release

---
+++ Dockerfile
@@ -0,0 +1,57 @@
+FROM        debian:stable-slim
+
+LABEL       MAINTAINER="https://github.com/Hermsi1337/"
+
+ARG         ARK_TOOLS_VERSION="1.6.42"
+ENV         LANG="en_US.UTF-8" \
+            LANGUAGE="en_US:en" \
+            LC_ALL="en_US.UTF-8" \
+            TERM="linux" \
+            SESSION_NAME="Dockerized ARK Server by github.com/hermsi1337" \
+            SERVER_MAP="TheIsland" \
+            SERVER_PASSWORD="YouShallNotPass" \
+            ADMIN_PASSWORD="topsecret" \
+            MAX_PLAYERS="20" \
+            UPDATE_ON_START="false" \
+            BACKUP_ON_STOP="false" \
+            WARN_ON_STOP="true" \
+            ARK_TOOLS_VERSION="${ARK_TOOLS_VERSION}" \
+            ARK_SERVER_VOLUME="/app" \
+            GAME_CLIENT_PORT="7777" \
+            RAW_UDP_PORT="7778" \
+            RCON_PORT="27020" \
+            SERVER_LIST_PORT="27015" \
+            STEAM_USER="steam" \
+            STEAM_UID="1000" \
+            STEAM_GID="1000" \
+            STEAM_HOME="/home/steam"
+
+RUN         set -x && \
+            apt-get -qq update && apt-get -qq upgrade && \
+            apt-get -qq install curl lib32gcc1 lsof perl-modules libc6-i386 bzip2 bash-completion locales sudo && \
+            sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen && locale-gen && \
+            addgroup --gid ${STEAM_GID} ${STEAM_USER} && \
+            adduser --home ${STEAM_HOME} --uid ${STEAM_UID} --gid ${STEAM_GID} --disabled-login --shell /bin/bash --gecos "" ${STEAM_USER} && \
+            echo "${STEAM_USER} ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers && \
+            usermod -a -G sudo ${STEAM_USER} && \
+            mkdir -p ${ARK_SERVER_VOLUME} ${STEAM_HOME}/steamcmd && \
+            curl -L https://github.com/FezVrasta/ark-server-tools/archive/v${ARK_TOOLS_VERSION}.tar.gz \
+                | tar -xvzf - -C /tmp/ && \
+            bash -c "cd /tmp/ark-server-tools-${ARK_TOOLS_VERSION}/tools && bash install.sh ${STEAM_USER}" && \
+            ln -s /usr/local/bin/arkmanager /usr/bin/arkmanager && \
+            curl -L http://media.steampowered.com/installer/steamcmd_linux.tar.gz \
+                | tar -xvzf - -C ${STEAM_HOME}/steamcmd/ && \
+            bash -x ${STEAM_HOME}/steamcmd/steamcmd.sh +login anonymous +quit && \
+            chown -R ${STEAM_USER}:${STEAM_USER} ${ARK_SERVER_VOLUME} && \
+            apt-get -qq autoclean && apt-get -qq autoremove && rm -rf /tmp/*
+
+ADD         conf.d/                     ${STEAM_HOME}/
+ADD         bin/                        ${STEAM_HOME}/
+ADD         conf.d/arkmanager-user.cfg  /etc/arkmanager/instances/main.cfg
+
+EXPOSE      ${GAME_CLIENT_PORT}/udp ${RAW_UDP_PORT}/udp ${STEAM_PORT}/upd ${RCON_PORT}/tcp
+
+VOLUME      ["${ARK_SERVER_VOLUME}"]
+WORKDIR     ${ARK_SERVER_VOLUME}
+
+ENTRYPOINT  ${STEAM_HOME}/user.sh
---
+++ bin/run.sh
@@ -0,0 +1,92 @@
+#!/usr/bin/env bash
+echo "_______________________________________"
+echo ""
+echo "# Ark Server - $(date)"
+echo "# UID ${STEAM_UID} - GID ${STEAM_GID}"
+echo "_______________________________________"
+
+function stop {
+	if [[ ${BACKUP_ON_STOP} == "true" ]] && [[ "$(ls -A server/ShooterGame/Saved/SavedArks)" ]]; then
+		echo "[Backup on stop]"
+		${ARKMANAGER} backup
+	fi
+	if [[ ${WARN_ON_STOP} == "true" ]];then
+	    ${ARKMANAGER} stop --warn
+	else
+	    ${ARKMANAGER} stop
+	fi
+	exit 0
+}
+
+function create_missing_dir {
+    for DIRECTORY in ${@}; do
+        [[ -n "${DIRECTORY}" ]] || return
+        if [[ ! -d ${DIRECTORY} ]]; then
+            mkdir -p ${DIRECTORY}
+            echo "Successfully created ${DIRECTORY}"
+        fi
+    done
+}
+
+
+[[ -p /tmp/FIFO ]] && rm /tmp/FIFO
+mkfifo /tmp/FIFO
+
+ARKMANAGER="$(command -v arkmanager)"
+[[ -n "${ARKMANAGER}" ]] || (echo "Arkamanger is missing" ; exit 1)
+
+# Change working directory to ${ARK_SERVER_VOLUME}
+cd ${ARK_SERVER_VOLUME}
+
+# Creating directory tree
+# Add a template directory to store the last version of config file
+create_missing_dir "${ARK_SERVER_VOLUME}/log" "${ARK_SERVER_VOLUME}/backup" "${ARK_SERVER_VOLUME}/staging" "${ARK_SERVER_VOLUME}/template"
+# We overwrite the template file each time
+cp ${STEAM_HOME}/arkmanager.cfg ${ARK_SERVER_VOLUME}/template/arkmanager.cfg
+cp ${STEAM_HOME}/crontab ${ARK_SERVER_VOLUME}/template/crontab
+# Create symbolic links
+[[ -f ${ARK_SERVER_VOLUME}/arkmanager.cfg ]] || cp ${ARK_SERVER_VOLUME}/template/arkmanager.cfg ${ARK_SERVER_VOLUME}/arkmanager.cfg
+[[ -L ${ARK_SERVER_VOLUME}/Game.ini ]] || ln -s server/ShooterGame/Saved/Config/LinuxServer/Game.ini Game.ini
+[[ -L ${ARK_SERVER_VOLUME}/GameUserSettings.ini ]] || ln -s server/ShooterGame/Saved/Config/LinuxServer/GameUserSettings.ini GameUserSettings.ini
+[[ -f ${ARK_SERVER_VOLUME}/crontab ]] || cp ${ARK_SERVER_VOLUME}/template/crontab ${ARK_SERVER_VOLUME}/crontab
+
+if [[ ! -d ${ARK_SERVER_VOLUME}/server  ]] || [[ ! -f ${ARK_SERVER_VOLUME}/server/arkversion ]];then
+	echo "No game files found. Installing..."
+	mkdir -p ${ARK_SERVER_VOLUME}/server/ShooterGame/Saved/SavedArks
+	mkdir -p ${ARK_SERVER_VOLUME}/server/ShooterGame/Content/Mods
+	mkdir -p ${ARK_SERVER_VOLUME}/server/ShooterGame/Binaries/Linux/
+	touch ${ARK_SERVER_VOLUME}/server/ShooterGame/Binaries/Linux/ShooterGameServer
+	${ARKMANAGER} install
+else
+	if [[ ${BACKUPONSTART} -eq 1 ]] && [[ "$(ls -A server/ShooterGame/Saved/SavedArks/)" ]]; then
+		echo "[Backup]"
+		${ARKMANAGER} backup
+	fi
+fi
+
+# If there is uncommented line in the file
+ACTIVE_CRONS="$(grep -v "^#" ${ARK_SERVER_VOLUME}/crontab 2>/dev/null | wc -l)"
+if [[ ${ACTIVE_CRONS} -gt 0 ]]; then
+	echo "Loading crontab..."
+	# We load the crontab file if it exist.
+	crontab ${ARK_SERVER_VOLUME}/crontab
+	# Cron is attached to this process
+	sudo cron -f &
+else
+	echo "No crontab set."
+fi
+
+# Launching ark server
+if [[ ${UPDATE_ON_START} == "false" ]]; then
+	${ARKMANAGER} start --noautoupdate
+else
+	${ARKMANAGER} start
+fi
+
+# Stop server in case of signal INT or TERM
+echo "Waiting..."
+trap stop INT
+trap stop TERM
+
+read < /tmp/FIFO &
+wait
