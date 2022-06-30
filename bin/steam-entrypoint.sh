ce18555bb569435906ec5f91a652cd0857d27ade
Dennis Hermsmeier <dennis@hermsmeier.me>
2022-06-30 10:02:29 +0200

    Major refactoring / issue tackling af (#57)
    * switch base img to steamcmd
    
    * add dev deploy
    
    * refactor for faster startup
    
    * add login for steam sessions
    
    * add hermsi_version for better debugging purposes
    
    * grammar
    
    * remove publish of dev img

--- bin/steam-entrypoint.sh
+++ bin/steam-entrypoint.sh
@@ -4,18 +4,10 @@ set -e
 
 [[ -z "${DEBUG}" ]] || [[ "${DEBUG,,}" = "false" ]] || [[ "${DEBUG,,}" = "0" ]] || set -x
 
-function ensure_rights() {
-  TARGET="${ARK_SERVER_VOLUME} ${STEAM_HOME}"
-  if [[ -n "${1}" ]]; then
-    TARGET="${1}"
-  fi
-
-  echo "...ensuring rights on ${TARGET}"
-  sudo chown -R "${STEAM_USER}":"${STEAM_GROUP}" ${TARGET} \
-    || echo "... FAILED ENSURING RIGHTS - CONTINUING STARTUP"
-    # we continue to prevent a boot-failure through read-only files
-    # https://github.com/Hermsi1337/docker-ark-server/issues/34
-}
+if [[ "$(whoami)" != "${STEAM_USER}" ]]; then
+  echo "run this script as steam-user"
+  exit 1
+fi
 
 function may_update() {
   if [[ "${UPDATE_ON_START}" != "true" ]]; then
@@ -24,7 +16,7 @@ function may_update() {
 
   echo "\$UPDATE_ON_START is 'true'..."
 
-  # auto checks if a update is needed, if yes, then update the server or mods 
+  # auto checks if a update is needed, if yes, then update the server or mods
   # (otherwise it just does nothing)
   ${ARKMANAGER} update --verbose --update-mods --backup --no-autostart
 }
@@ -35,7 +27,6 @@ function create_missing_dir() {
     if [[ ! -d "${DIRECTORY}" ]]; then
       mkdir -p "${DIRECTORY}"
       echo "...successfully created ${DIRECTORY}"
-      ensure_rights "${DIRECTORY}"
     fi
   done
 }
@@ -48,31 +39,22 @@ function copy_missing_file() {
     cp -a "${SOURCE}" "${DESTINATION}"
     echo "...successfully copied ${SOURCE} to ${DESTINATION}"
   fi
-
-  ensure_rights ${DESTINATION}
 }
-
-if [[ ! "$(id -u "${STEAM_USER}")" -eq "${STEAM_UID}" ]] || [[ ! "$(id -g "${STEAM_GROUP}")" -eq "${STEAM_GID}" ]]; then
-  sudo usermod -o -u "${STEAM_UID}" "${STEAM_USER}"
-  sudo groupmod -o -g "${STEAM_GID}" "${STEAM_GROUP}"
-fi
-
-# Always ensure correct rights on home and volume folder
-ensure_rights ""
 
 args=("$*")
 if [[ "${ENABLE_CROSSPLAY}" == "true" ]]; then
-  args=('--arkopt,-crossplay' "${args[@]}");
+  args=('--arkopt,-crossplay' "${args[@]}")
 fi
 if [[ "${DISABLE_BATTLEYE}" == "true" ]]; then
-  args=('--arkopt,-NoBattlEye' "${args[@]}");
+  args=('--arkopt,-NoBattlEye' "${args[@]}")
 fi
 
 echo "_______________________________________"
 echo ""
 echo "# Ark Server - $(date)"
-echo "# UID ${STEAM_UID} - GID ${STEAM_GID}"
-echo "# ARGS ${args[*]}"
+echo "# HERMSI_VERSION: '${HERMSI_VERSION}'"
+echo "# RUNNING AS USER '${STEAM_USER}'"
+echo "# ARGS: ${args[*]}"
 echo "_______________________________________"
 
 ARKMANAGER="$(command -v arkmanager)"
@@ -85,19 +67,6 @@ cd "${ARK_SERVER_VOLUME}"
 
 echo "Setting up folder and file structure..."
 create_missing_dir "${ARK_SERVER_VOLUME}/log" "${ARK_SERVER_VOLUME}/backup" "${ARK_SERVER_VOLUME}/staging"
-
-
-echo "Setting up Arkmanager..."
-# setup arkmanager directories
-if [[ ! -d ${ARK_TOOLS_DIR} ]]; then
-  sudo mv "/etc/arkmanager" "${ARK_TOOLS_DIR}"
-  ensure_rights "${ARK_TOOLS_DIR}"
-  rm -f "${ARK_TOOLS_DIR}/arkmanager.cfg" "${ARK_TOOLS_DIR}/instances/main.cfg"
-fi
-
-# symlink arkmanager directories
-sudo rm -rf "/etc/arkmanager"
-sudo ln -s "${ARK_TOOLS_DIR}" "/etc/arkmanager"
 
 # copy from template to server volume
 copy_missing_file "${TEMPLATE_DIRECTORY}/arkmanager.cfg" "${ARK_TOOLS_DIR}/arkmanager.cfg"
@@ -121,19 +90,9 @@ if [[ ! -d ${ARK_SERVER_VOLUME}/server ]] || [[ ! -f ${ARK_SERVER_VOLUME}/server
   chmod +x "${ARK_SERVER_VOLUME}/server/ShooterGame/Binaries/Linux/ShooterGameServer"
 
   ${ARKMANAGER} install --verbose
-else
-  may_update
 fi
 
-ACTIVE_CRONS="$(grep -v "^#" "${ARK_SERVER_VOLUME}/crontab" 2>/dev/null | wc -l)"
-if [[ ${ACTIVE_CRONS} -gt 0 ]]; then
-  echo "Loading crontab..."
-  crontab "${ARK_SERVER_VOLUME}/crontab"
-  sudo service cron start
-  echo "...done"
-else
-  echo "No crontab set"
-fi
+crontab "${ARK_SERVER_VOLUME}/crontab"
 
 if [[ -n "${GAME_MOD_IDS}" ]]; then
   echo "Installing mods: '${GAME_MOD_IDS}' ..."
@@ -151,4 +110,6 @@ if [[ -n "${GAME_MOD_IDS}" ]]; then
   done
 fi
 
-exec ${ARKMANAGER} run --verbose ${args[@]}
+may_update
+
+exec "${ARKMANAGER}" run --verbose ${args[@]}
