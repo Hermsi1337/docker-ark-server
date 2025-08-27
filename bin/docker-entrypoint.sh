5be64f205668e76c7d1c6a4e9cfe8a941a21ae57
MusclePr <muscle@a-b-c-d.com>
2025-08-27 09:40:21 +0900

    fix chown warnings.

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -8,14 +8,14 @@ if [[ ! -d "${ARK_SERVER_VOLUME}" ]]; then
   mkdir -p "${ARK_SERVER_VOLUME}"
 fi
 
-chown "${STEAM_USER}". "${ARK_SERVER_VOLUME}" || echo "Failed setting rights on ${ARK_SERVER_VOLUME}, continuing startup..."
+chown "${STEAM_USER}": "${ARK_SERVER_VOLUME}" || echo "Failed setting rights on ${ARK_SERVER_VOLUME}, continuing startup..."
 
 if [[ ! -d ${ARK_TOOLS_DIR} ]]; then
   mv "/etc/arkmanager" "${ARK_TOOLS_DIR}"
   rm -f "${ARK_TOOLS_DIR}/arkmanager.cfg" "${ARK_TOOLS_DIR}/instances/main.cfg"
 fi
 
-chown -R "${STEAM_USER}". "${ARK_TOOLS_DIR}" || echo "Failed setting rights on ${ARK_TOOLS_DIR}, continuing startup..."
+chown -R "${STEAM_USER}": "${ARK_TOOLS_DIR}" || echo "Failed setting rights on ${ARK_TOOLS_DIR}, continuing startup..."
 
 # symlink arkmanager directories
 rm -rf "/etc/arkmanager"
