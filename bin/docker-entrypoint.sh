4e0fad5556a76d3fdcad6c50b60600e15c268565
MusclePr <muscle@a-b-c-d.com>
2025-10-03 18:44:14 +0900

    Auto generate arkmanager/instances/sub.<KEY>.cfg from enviroments.

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -4,11 +4,10 @@ set -e
 
 [[ -z "${DEBUG}" ]] || [[ "${DEBUG,,}" = "false" ]] || [[ "${DEBUG,,}" = "0" ]] || set -x
 
-if [[ ! -d "${ARK_SERVER_VOLUME}" ]]; then
-  mkdir -p "${ARK_SERVER_VOLUME}"
-fi
-
-chown "${STEAM_USER}": "${ARK_SERVER_VOLUME}" || echo "Failed setting rights on ${ARK_SERVER_VOLUME}, continuing startup..."
+for dir in "${ARK_SERVER_VOLUME}" "/cluster"; do
+  mkdir -p "${dir}"
+  chown "${STEAM_USER}": "${dir}" || echo "Failed setting rights on ${dir}, continuing startup..."
+done
 
 if [[ ! -d ${ARK_TOOLS_DIR} ]]; then
   mv "/etc/arkmanager" "${ARK_TOOLS_DIR}"
@@ -17,16 +16,10 @@ fi
 
 chown -R "${STEAM_USER}": "${ARK_TOOLS_DIR}" || echo "Failed setting rights on ${ARK_TOOLS_DIR}, continuing startup..."
 
-if [[ ! -d /cluster ]]; then
-  mkdir -p "/cluster"
-fi
-
-chown "${STEAM_USER}": "/cluster" || echo "Failed setting rights on /cluster, continuing startup..."
-
 # symlink arkmanager directories
 rm -rf "/etc/arkmanager"
 ln -s "${ARK_TOOLS_DIR}" "/etc/arkmanager"
 
 service cron start
 
-exec gosu "${STEAM_USER}" /steam-entrypoint.sh $*
+exec gosu "${STEAM_USER}" /steam-entrypoint.sh "$@"
